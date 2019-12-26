ef033e3e3f7fb00ec5e1757a40e8436c99f283ae
Ilya Yurov <54723869+Frit027@users.noreply.github.com>
2019-12-26 13:40:16 +0300

    Delete type-search-index.js

--- Prac№8/src/docs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.mirea","l":"BoundedWaitList"},{"p":"com.mirea","l":"IWaitList"},{"p":"com.mirea","l":"Main"},{"p":"com.mirea","l":"UnfairWaitList"},{"p":"com.mirea","l":"WaitList"}]
